4cd6c09a004654f7e4c098a4d4318fe97fe55439
Neematrasheed05 <nymatrasheed05@gmail.com>
2023-12-12 09:33:53 +0300

    alembic issues

--- seed.py
+++ seed.py
@@ -1,19 +1,25 @@
-from models import Restaurant, Review, Customer
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model import Restaurant,Review,Customer
+
+
+if __name__ == '__main__':
+    
+    engine = create_engine('sqlite:///restaurants.db')
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
 # Create some restaurants
-restaurant1 = Restaurant(name="Pizza Palace", price=10)
-restaurant2 = Restaurant(name="Fancy French", price=25)
+restaurant1 = Restaurant(name="Pizza inn", price=10)
+restaurant2 = Restaurant(name="pronto", price=25)
 
 # Create some customers
 customer1 = Customer(first_name="Jane", last_name="Doe")
 customer2 = Customer(first_name="John", last_name="Smith")
 
-# Add reviews
-customer1.add(restaurant1, 5)
-customer1.add(restaurant2, 4)
-customer2.add(restaurant1, 3)
-customer2.add(restaurant2, 5)
 
-# Add to session and commit
-session.add_all([restaurant1, restaurant2, customer1, customer2])
+review1 = Review(star_rating=2, restaurant=restaurant1, customer=customer1)
+review2 = Review(star_rating=4, restaurant=restaurant2, customer=customer2)
+
+session.add_all([restaurant1, restaurant2, customer1, customer2, review1, review2])
 session.commit()
