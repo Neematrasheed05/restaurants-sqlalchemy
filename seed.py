eb1c3b00a50dc5047d3cf70fa97a56b239b20927
Neematrasheed05 <nymatrasheed05@gmail.com>
2023-12-12 08:44:52 +0300

    created model.py, and seed.py

--- seed.py
+++ seed.py
@@ -9,10 +9,10 @@ customer1 = Customer(first_name="Jane", last_name="Doe")
 customer2 = Customer(first_name="John", last_name="Smith")
 
 # Add reviews
-customer1.add_review(restaurant1, 5)
-customer1.add_review(restaurant2, 4)
-customer2.add_review(restaurant1, 3)
-customer2.add_review(restaurant2, 5)
+customer1.add(restaurant1, 5)
+customer1.add(restaurant2, 4)
+customer2.add(restaurant1, 3)
+customer2.add(restaurant2, 5)
 
 # Add to session and commit
 session.add_all([restaurant1, restaurant2, customer1, customer2])
